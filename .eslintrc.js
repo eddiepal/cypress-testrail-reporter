d5cfec7a65ac27986f1cb8f910d97d86fdb5e4c1
Eddie <empburke@outlook.com>
2023-01-17 10:11:34 +0000

    Added missing eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'rules': {
+    'max-len': ['error', {'code': 120}],
+  },
+};
