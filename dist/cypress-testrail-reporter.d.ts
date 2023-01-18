fbd85a888730445f4cf32606c2d28aafaf9d1ec0
Eddie <empburke@outlook.com>
2023-01-18 22:08:46 +0000

    updating dist folder, adding to eslint ignore

---
+++ dist/cypress-testrail-reporter.d.ts
@@ -0,0 +1,25 @@
+import { reporters } from 'mocha';
+export declare class CypressTestRailReporter extends reporters.Spec {
+    private results;
+    private testRailApi;
+    private testRailValidation;
+    private runId;
+    private reporterOptions;
+    private suiteId;
+    private groupId;
+    constructor(runner: any, options: any);
+    /**
+     * @param {int} status The test status.
+     * @param {object} test The test object.
+     * @param {string} comment The test comment.
+   */
+    addToResults: (status: number, test: any, comment: string) => Promise<void>;
+    /**
+     * Ensure that after each test results are reported continuously
+     * Additionally to that if test status is failed or retried there is possibility
+     * to upload failed screenshot for easier debugging in TestRail
+     * Note: Uploading of screenshot is configurable option
+     * @return {any}
+   */
+    submitResults: () => Promise<void>;
+}
